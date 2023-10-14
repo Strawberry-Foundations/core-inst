f3c07e37b56fd8f9611b4a2451f83970e7d8bd0d
Juliandev02 <juli.mackenstein@gmail.com>
2023-10-14 21:33:26 +0200

    added help command

--- copt/copt.py
+++ copt/copt.py
@@ -26,13 +26,27 @@ from sys import argv
 
 arg_len = len(argv)
 
+package_list = ["sys-base", "sys-base-dev"]
+
 match arg_len > 1 and argv[1]:
+    case "help":
+        print(help_message)
+    
     case "install":
         if not arg_len > 2:
-            exit()
+            exit(1)
         
-        if not os.path.exists(directories.var_spkg) and not os.path.exists(directories.base_spkg):
-            print("")
+        if not os.path.exists(directories.var_spkg) or not os.path.exists(directories.base_spkg):
+            print(f"{RED + BOLD}E: {CRESET}spkg was not found on this system. Please consider installing spkg before using copt!")
+            exit(1)
         
         bootstrap_list = argv[2:]
+        not_found_packages = [item for item in bootstrap_list if item not in package_list]
+
+        
+        if not_found_packages:
+            print(f"{RED + BOLD}E: {CRESET}Following packages were not found: {not_found_packages}")
+            exit()
+            
+            
         print(bootstrap_list)
