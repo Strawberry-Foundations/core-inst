d19078825c432e52b34fe720d3f3f4ab13f8eef3
Juliandev02 <juli.mackenstein@gmail.com>
2023-10-14 21:33:31 +0200

    added help message

--- copt/src/__init__.py
+++ copt/src/__init__.py
@@ -15,11 +15,35 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
 import os
+import platform
 
+from src.colors import *
 
 version = "1.0.0"
 
 class directories:
     var_spkg    = "/var/lib/spkg/"
     base_spkg   = "/usr/share/spkg/"
+
+help_message = f"""{GREEN}{BOLD}coreOS Package Tool{CRESET} ({version}) {platform.machine()}
+
+{CYAN}{BOLD}Usage:{RESET} copt {GREEN}[Command] {RED}<Argument>
+
+{BOLD}{MAGENTA}-> Package installation, update and removal{CRESET}
+    {CYAN}{BOLD}-a{CRESET}      Installs packages
+    {CYAN}{BOLD}-d{CRESET}      Removes packages
+    {CYAN}{BOLD}-u{CRESET}      Updates packages   
     
+{BOLD}{CYAN}-> Package information{CRESET}
+    {CYAN}{BOLD}-i{CRESET}      Gives information about packages
+    {CYAN}{BOLD}-l{CRESET}      Lists packages according to a certain criterion
+
+{BOLD}{YELLOW}-> Repository management{CRESET}
+    {CYAN}{BOLD}-s{CRESET}      Syncs repository
+
+{BOLD}{RED}-> Miscellaneous{CRESET}
+    {CYAN}{BOLD}-Ab{CRESET}     About page for copt
+
+{BOLD}{BLUE}-> Bootstrapping{CRESET}
+    {CYAN}{BOLD}-Cb{CRESET}     Bootstrapps your system according to the passed argumente
+"""
